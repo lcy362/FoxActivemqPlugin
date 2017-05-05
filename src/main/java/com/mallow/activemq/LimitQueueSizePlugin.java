21509642597fe46b8e6fa4bb2e3a1f8eb09b3e45
lcy362 <lcy362@163.com>
2017-05-05 11:22:31 +0800

    comment

--- src/main/java/com/mallow/activemq/LimitQueueSizePlugin.java
+++ src/main/java/com/mallow/activemq/LimitQueueSizePlugin.java
@@ -5,6 +5,7 @@ import org.apache.activemq.broker.BrokerPlugin;
 
 /**
  * Created by lcy on 2016/4/26.
+ * A plugin can be useful in test enviroment, which will discard all messages when there are more than 1000 messages pending
  */
 public class LimitQueueSizePlugin implements BrokerPlugin {
     public Broker installPlugin(Broker broker) throws Exception {
