02ab072bcd5c57a00993f01552437c6e72372bc3
lcy362 <lcy362@163.com>
2017-05-05 11:14:23 +0800

    get queue size

--- src/main/java/com/mallow/activemq/LimitQueueSIzeBroker.java
+++ src/main/java/com/mallow/activemq/LimitQueueSIzeBroker.java
@@ -2,31 +2,48 @@ package com.mallow.activemq;
 
 import org.apache.activemq.broker.Broker;
 import org.apache.activemq.broker.BrokerFilter;
-import org.apache.activemq.broker.ConsumerBrokerExchange;
 import org.apache.activemq.broker.ProducerBrokerExchange;
+import org.apache.activemq.broker.region.Destination;
+import org.apache.activemq.broker.region.DestinationStatistics;
 import org.apache.activemq.command.ActiveMQDestination;
 import org.apache.activemq.command.Message;
-import org.apache.activemq.command.MessageAck;
-import org.apache.activemq.command.MessageDispatch;
 import org.apache.log4j.Logger;
 
-import java.util.Queue;
+import java.util.Set;
+
 
 /**
  * Created by lcy on 2016/4/26.
  */
 public class LimitQueueSIzeBroker extends BrokerFilter {
     Logger logger = Logger.getLogger("test");
+
     public LimitQueueSIzeBroker(Broker next) {
         super(next);
     }
 
     @Override
     public void send(ProducerBrokerExchange producerExchange, Message messageSend) throws Exception {
-        int nowsize = 0;
-        ActiveMQDestination destination = messageSend.getDestination();
-        System.out.println(destination);
-        super.send(producerExchange, messageSend);
+        ActiveMQDestination msgDest = messageSend.getDestination();
+        String physicalName = msgDest.getPhysicalName();
+        logger.info("dest: " + physicalName);
+
+        ActiveMQDestination queryDestination = ActiveMQDestination.createDestination(physicalName, msgDest.getDestinationType());
+        Set<Destination> destinations = getDestinations(queryDestination);
+        logger.info(destinations);
+        long count = 0;
+        for (Destination dest : destinations) {
+            DestinationStatistics stats = dest.getDestinationStatistics();
+            if (stats != null) {
+                count = stats.getMessageSize().getCount();
+                logger.info("size: " + count);
+            }
+        }
+        if (count < 1000) {
+            super.send(producerExchange, messageSend);
+        } else {
+            logger.info("discard message");
+        }
     }
 
 }
